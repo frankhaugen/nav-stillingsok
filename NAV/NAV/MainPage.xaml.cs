d48dece89b46f855c949a250e345683076d3355e
frank <frank.haugen@gmail.com>
2018-10-29 05:19:46 +0100

    Tested displaying og XML feed

--- NAV/NAV/MainPage.xaml.cs
+++ NAV/NAV/MainPage.xaml.cs
@@ -7,16 +7,40 @@ using Xamarin.Forms;
 
 using System.Xml;
 using Xamarin.Essentials;
+using System.Diagnostics;
 
 namespace NAV
 {
 	public partial class MainPage : ContentPage
 	{
+
 		public MainPage()
 		{
 			InitializeComponent();
+			DisplayXML(CollectFeed());
 
-			 
+
+		}
+
+		private XmlDocument CollectFeed(string rssfeed = @"https://tjenester.nav.no/stillinger/rss?p=0&rpp=5&rv=al&sort=akt")
+		{
+			XmlDocument output = new XmlDocument();
+
+			try
+			{
+				output.Load(rssfeed);
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine(e.Message);
+			}
+			
+			return output;
+		}
+
+		private void DisplayXML(XmlDocument input)
+		{
+			OutputLabel.Text = input.InnerXml;
 		}
 	}
 }
